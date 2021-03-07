61f82e9e14e78fade2d714e849bd631a85ddee59
steven caraballo <steven011004@gmail.com>
2021-03-07 11:42:52 -0400

    BotonesnumPuestos

--- Calculadora/Form1.cs
+++ Calculadora/Form1.cs
@@ -17,5 +17,84 @@ namespace Calculadora
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TxbPantalla.Text = TxbPantalla.Text + "7";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            TxbPantalla.Text = TxbPantalla.Text + "9";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            TxbPantalla.Text = TxbPantalla.Text + "5";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            TxbPantalla.Text = TxbPantalla.Text + "0";
+        }
+
+        private void BtnN1_Click(object sender, EventArgs e)
+        {
+            TxbPantalla.Text = TxbPantalla.Text + "1";
+        }
+
+        private void BtnN2_Click(object sender, EventArgs e)
+        {
+            TxbPantalla.Text = TxbPantalla.Text + "2";
+        }
+
+        private void BtnN3_Click(object sender, EventArgs e)
+        {
+            TxbPantalla.Text = TxbPantalla.Text + "3";
+        }
+
+        private void BtnN4_Click(object sender, EventArgs e)
+        {
+            TxbPantalla.Text = TxbPantalla.Text + "4";
+        }
+
+        private void BtnN6_Click(object sender, EventArgs e)
+        {
+            TxbPantalla.Text = TxbPantalla.Text + "6";
+        }
+
+        private void BtnN8_Click(object sender, EventArgs e)
+        {
+            TxbPantalla.Text = TxbPantalla.Text + "8";
+        }
     }
 }
